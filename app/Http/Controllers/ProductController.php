f4b570c35945024c1af92e057cf970a157274de9
Lucas Vergara <lucas.ivergara18@gmail.com>
2021-12-20 01:19:41 -0300

    finished

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Category;
 use App\Models\Product;
 use App\Models\Provider;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -13,7 +14,8 @@ class ProductController extends Controller
 
     public function deleteProduct($id)
     {
-        Product::destroy($id);
+        $flight = User::find($id);
+        $flight->delete();
         return ['ok' => true];
     }
 
